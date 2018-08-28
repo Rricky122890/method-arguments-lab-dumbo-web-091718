ef126e63d11292eb397fb9dc15d9928a364878e2
Ricardo Jean-Pierre <rjeanpierre90@gmail.com>
2018-08-28 17:39:45 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,10 @@
+def introduction(name)
+puts "Hi! My name is #{name}."
+
+end
+
+introduction(Rico)
+
+def introduction_with_language(name, lang)
+ puts "Hi! My name is #{name} and I am learning to program in #{lang}"
+end
